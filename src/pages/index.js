ed39408dbb5efa2d445d8a439defa2d0524f2bed
Dmitriy Seregin <d.seregin@adguard.com>
2021-08-18 17:21:54 +0300

    add some adguard content

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            AdGuard Knowledgebase - 🔰
           </Link>
         </div>
       </div>
