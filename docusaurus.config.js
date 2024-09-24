a6a9ed9af87bc10097c6d4573f91e66763131b83
Ildar Kamalov <ik@adguard.com>
2024-09-24 11:20:01 +0300

    ADG-9088 add pt-br locale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ module.exports = {
   themes: ['docusaurus-theme-search-typesense'],
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ru', 'de', 'cs', 'fr', 'es', 'it', 'ja', 'ko', 'zh-CN', 'zh-TW'],
+    locales: ['en', 'ru', 'de', 'cs', 'fr', 'es', 'it', 'ja', 'ko', 'zh-CN', 'zh-TW', 'pt-BR'],
   },
   themeConfig: {
     colorMode: {
